84b983ed5d349cd13c5bc3c00da9eb288ca38a66
민형기 <alsvhtks@naver.com>
2022-12-10 23:34:22 +0900

    3

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   let [logo, setLogo] = useState("ReactBlog");
   let [date, setDate] = useState("2월 17일 발행");
   let [good, setGood] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -37,27 +38,33 @@ function App() {
         <p>{date}</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4
+          onClick={
+            modal == false
+              ? function () {
+                  setModal(true);
+                }
+              : function () {
+                  setModal(false);
+                }
+          }
+        >
+          {글제목[2]}
+        </h4>
         <p>{date}</p>
-        <button
-          onClick={() => {
-            let copy = [...글제목];
-            copy[0] = "여자코트 추천";
-            글변경(copy);
-          }}
-        >
-          글바꾸기
-        </button>
-        <button
-          onClick={() => {
-            let copy = [...글제목];
-            copy = copy.sort();
-            글변경(copy);
-          }}
-        >
-          가나다순정렬
-        </button>
       </div>
+
+      {modal == true ? <Modal /> : ""}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
