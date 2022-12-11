86e2efcb58cd09b213121820f0763b34591189c3
민형기 <alsvhtks@naver.com>
2022-12-11 17:10:06 +0900

    5

--- src/App.js
+++ src/App.js
@@ -19,31 +19,25 @@ function App() {
       <div className="black-nav">
         <h4>{logo}</h4>
       </div>
-      <div className="list">
-        <h4>{글제목[0]}</h4>
-        <p>{date}</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>{date}</p>
-      </div>
-      <div className="list">
-        <h4
-          onClick={
-            modal == false
-              ? function () {
-                  setModal(true);
-                }
-              : function () {
-                  setModal(false);
-                }
-          }
-        >
-          {글제목[2]}
-        </h4>
-        <p>{date}</p>
-      </div>
-      {modal == true ? <Modal 글변경={글변경} 글제목={글제목} /> : ""}
+      {글제목.map(function (a, i) {
+        return (
+          <div className="modal">
+            <h4
+              onClick={() => {
+                setModal(true);
+              }}
+            >
+              {글제목[i]}
+            </h4>
+            <p>{date}</p>
+          </div>
+        );
+      })}
+      {modal == true ? (
+        <Modal date={date} 글변경={글변경} 글제목={글제목} />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
@@ -51,8 +45,14 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h4>{props.글제목[0]}</h4>
-      <p>날짜</p>
+      <h4>
+        {() => {
+          let copy = [...props.글제목];
+          copy[0] = props.글제목[0];
+          props.글변경(copy);
+        }}
+      </h4>
+      <p>{props.date}</p>
       <p>상세내용</p>
       <button
         onClick={() => {
