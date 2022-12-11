7dcfa9fb9c94c0856f7924fb7c13f3fcfc74f2e9
민형기 <alsvhtks@naver.com>
2022-12-11 23:44:43 +0900

    6

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { getByTitle } from "@testing-library/react";
 
 function App() {
-  let post = "강남우동맛집";
-  let [글제목, 글변경] = useState([
-    "남자 코트 추천",
+  let [title, setTitle] = useState([
     "강남 우동 맛집",
-    "파이썬 독학",
+    "파주 우동 맛집",
+    "일산 우동 맛집",
   ]);
-  let [logo, setLogo] = useState("ReactBlog");
-  let [date, setDate] = useState("2월 17일 발행");
-  let [good, setGood] = useState(0);
+  let [date, setDate] = useState([
+    "2022년 12월 10일",
+    "2022년 12월 11일",
+    "2022년 12월 12일",
+  ]);
+  let [good, setGood] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
-
+  let [modaltitle, setModaltitle] = useState(["강남", "파주", "일산"]);
   return (
-    <div className="App">
-      <div className="black-nav">
-        <h4>{logo}</h4>
-      </div>
-      {글제목.map(function (a, i) {
+    <div className="wrap">
+      <div className="black-nav">ReactCoding</div>
+      {title.map(function (a, i) {
         return (
-          <div className="modal">
-            <h4
+          <div className="list" key={i}>
+            <span
               onClick={() => {
-                setModal(true);
+                modal == false ? setModal(true) : setModal(false);
               }}
             >
-              {글제목[i]}
-            </h4>
-            <p>{date}</p>
+              {title[i]}
+            </span>
+            <span>{date[i]}</span>
+            <span
+              onClick={() => {
+                let goodCnt = [...good];
+                goodCnt[i]++;
+                setGood(goodCnt);
+              }}
+            >
+              좋아요
+            </span>
+            <span>{good[i]}</span>
           </div>
         );
       })}
       {modal == true ? (
-        <Modal date={date} 글변경={글변경} 글제목={글제목} />
+        <Modal
+          title={title}
+          modal={modal}
+          modaltitle={modaltitle}
+          date={date}
+        />
       ) : (
         ""
       )}
@@ -45,26 +61,9 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h4>
-        {() => {
-          let copy = [...props.글제목];
-          copy[0] = props.글제목[0];
-          props.글변경(copy);
-        }}
-      </h4>
-      <p>{props.date}</p>
-      <p>상세내용</p>
-      <button
-        onClick={() => {
-          let copy = [...props.글제목];
-          copy[0] = "여자 코트 추천";
-          props.글변경(copy);
-        }}
-      >
-        첫번째 글 변경
-      </button>
+      <h4>{props.title[0]}</h4>
+      <span>{props.date[0]}</span>
     </div>
   );
 }
-
 export default App;
