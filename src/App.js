ca2f010ea4c71f7a23e2256ebc2d6135f0d907a1
민형기 <alsvhtks@naver.com>
2022-12-11 10:03:47 +0900

    4

--- src/App.js
+++ src/App.js
@@ -20,17 +20,7 @@ function App() {
         <h4>{logo}</h4>
       </div>
       <div className="list">
-        <h4>
-          {글제목[0]}{" "}
-          <span
-            onClick={function () {
-              setGood(good + 1);
-            }}
-          >
-            좋아요
-          </span>{" "}
-          {good}
-        </h4>
+        <h4>{글제목[0]}</h4>
         <p>{date}</p>
       </div>
       <div className="list">
@@ -53,18 +43,26 @@ function App() {
         </h4>
         <p>{date}</p>
       </div>
-
-      {modal == true ? <Modal /> : ""}
+      {modal == true ? <Modal 글변경={글변경} 글제목={글제목} /> : ""}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button
+        onClick={() => {
+          let copy = [...props.글제목];
+          copy[0] = "여자 코트 추천";
+          props.글변경(copy);
+        }}
+      >
+        첫번째 글 변경
+      </button>
     </div>
   );
 }
